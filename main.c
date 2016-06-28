426a6da0d96079e0bb0eb32cc72554172badd436
Simon Maurer <moirelein@gmail.com>
2016-06-28 15:16:39 +0100

    usage and cmd line options

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include "defines.h"
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 FILE* __src_file;
 
@@ -40,37 +41,61 @@ int main( int argc, char **argv )
 {
     igraph_t g;
     const char* src_file_name;
-    char* file_path;
+    char* out_path = NULL;
     char* file_name;
     char* path_main;
     char* path_boxh;
     char* path_boxc;
     FILE* ifile;
     int path_size, name_size;
+    int c;
     igraph_i_set_attribute_table( &igraph_cattribute_table );
 
-    if( argc != 2 ) {
-        printf( "Missing argument!\n" );
+    while( ( c = getopt( argc, argv, "hvp:" ) ) != -1 )
+        switch( c ) {
+            case 'h':
+                printf( "Usage:\n  %s [OPTION...] FILE\n\n", argv[0] );
+                printf( "Options:\n" );
+                printf( "  -h            This message\n" );
+                printf( "  -v            Version\n" );
+                printf( "  -p 'path'     Path to store the generated files\n" );
+                return 0;
+            case 'v':
+                printf( "gml2c-v0.0.1\n" );
+                return 0;
+            case 'p':
+                out_path = optarg;
+                break;
+            case '?':
+                if ( optopt == 'p' )
+                    fprintf ( stderr, "Option -%c requires an argument.\n",
+                            optopt );
+                else
+                    fprintf ( stderr, "Unknown option character `\\x%x'.\n",
+                            optopt );
+                return 1;
+            default:
+                abort();
+        }
+    if( argc <= optind ) {
+        fprintf( stderr, "Missing argument!\n" );
         return -1;
     }
-    src_file_name = argv[ 1 ];
+    src_file_name = argv[ optind ];
+    if( out_path == NULL ) out_path = "";
 
     path_size = get_path_size( src_file_name );
-    file_path = malloc( path_size + 1 );
-    memcpy( file_path, src_file_name, path_size );
-    file_path[ path_size ] = '\0';
-
     name_size = get_name_size( src_file_name );
     file_name = malloc( name_size + 1 );
     memcpy( file_name, &src_file_name[ path_size ], name_size );
     file_name[ name_size ] = '\0';
 
-    path_main = malloc( strlen( file_name ) + strlen( file_path ) + 3 );
-    sprintf( path_main, "%s%s.c", file_path, file_name );
-    path_boxh = malloc( strlen( FILE_BOX_H ) + strlen( file_path ) + 1 );
-    sprintf( path_boxh, "%s%s", file_path, FILE_BOX_H );
-    path_boxc = malloc( strlen( FILE_BOX_C ) + strlen( file_path ) + 1 );
-    sprintf( path_boxc, "%s%s", file_path, FILE_BOX_C );
+    path_main = malloc( strlen( file_name ) + strlen( out_path ) + 3 );
+    sprintf( path_main, "%s%s.c", out_path, file_name );
+    path_boxh = malloc( strlen( FILE_BOX_H ) + strlen( out_path ) + 1 );
+    sprintf( path_boxh, "%s%s", out_path, FILE_BOX_H );
+    path_boxc = malloc( strlen( FILE_BOX_C ) + strlen( out_path ) + 1 );
+    sprintf( path_boxc, "%s%s", out_path, FILE_BOX_C );
 
     ifile = fopen( src_file_name, "r" );
     igraph_read_graph_gml( &g, ifile );
@@ -90,10 +115,9 @@ int main( int argc, char **argv )
 
     /* printf( "str( %lu ): %s\n", strlen( src_file_name ), src_file_name ); */
     /* printf( "name( %lu/%d ): %s\n", strlen( file_name ), name_size, file_name ); */
-    /* printf( "path( %lu/%d ): %s\n", strlen( file_path ), path_size, file_path ); */
+    /* printf( "path( %lu/%d ): %s\n", strlen( out_path ), path_size, out_path ); */
 
     free( file_name );
-    free( file_path );
     free( path_main );
     free( path_boxh );
     free( path_boxc );
